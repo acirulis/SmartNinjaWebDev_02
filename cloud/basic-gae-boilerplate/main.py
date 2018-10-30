bb046dd52160f2d0f429762a51b0973c8acd87f9
Andis Cirulis <andis.cirulis@whitedigital.eu>
2018-10-30 18:56:39 +0200

    CONTACTS

--- cloud/basic-gae-boilerplate/main.py
+++ cloud/basic-gae-boilerplate/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals # pirmās divas rindiņas vajadzīgas lai Python2 + webapp2 atbalstītu latviešu burtis
+from __future__ import \
+    unicode_literals  # pirmās divas rindiņas vajadzīgas lai Python2 + webapp2 atbalstītu latviešu burtis
 import webapp2
 import os
 import jinja2
@@ -9,6 +10,7 @@ import jinja2
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
 
 class BaseHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -27,10 +29,11 @@ class BaseHandler(webapp2.RequestHandler):
         template = jinja_env.get_template(view_filename)
         self.response.out.write(template.render(params))
 
+
 class MainHandler(BaseHandler):
     def get(self):
         list = '<ul>'
-        for a in range(1,10):
+        for a in range(1, 10):
             list += '<li>Kaut kas: ' + str(a) + '</li>'
         list += '</ul>'
         params = {
@@ -40,12 +43,14 @@ class MainHandler(BaseHandler):
         }
         return self.render_template('index.html', params)
 
+
 class AboutHandler(BaseHandler):
     def get(self):
         params = {
             'page_title': 'Par mums'
         }
         return self.render_template('about.html', params)
+
 
 class DatabaseHandler(BaseHandler):
     def get(self):
@@ -61,14 +66,20 @@ class DatabaseHandler(BaseHandler):
         from models import Message
         msg_text = self.request.get("message_text")
         msg_notes = self.request.get("message_notes")
-        msg = Message(message_text=msg_text, message_notes=msg_notes)
-        msg.put()
-        messages = Message.query().fetch()
-        params = {
-            'page_title': 'Database',
-            'status': 'Added!',
-            'messages': messages,
-        }
+        email = self.request.get("email")
+        skaitlis = self.request.get("skaitlis")
+        if skaitlis.isdigit():
+            skaitlis = int(skaitlis)
+            msg = Message(message_text=msg_text, message_notes=msg_notes, email=email, skaitlis=skaitlis)
+            msg.put()
+            status = 'Addded successfully!'
+        else:
+            status = 'Please enter number!'
+        messages = Message.query().order(-Message.created).fetch()
+        params = {'status': status,
+                  'messages': messages,
+                  'page_title': 'Databases',
+                  }
         return self.render_template('database.html', params)
 
 
@@ -78,11 +89,12 @@ class FormHandler(BaseHandler):
             'page_title': 'Forma'
         }
         return self.render_template('form.html', params)
+
     def post(self):
         skaitlis = self.request.get('skaitlis')
-        kvadrats = int(skaitlis)**2
+        kvadrats = int(skaitlis) ** 2
         if skaitlis.isdigit():
-            kvadrats = int(skaitlis)**2
+            kvadrats = int(skaitlis) ** 2
         else:
             kvadrats = 0
         params = {
@@ -94,9 +106,57 @@ class FormHandler(BaseHandler):
         }
         return self.render_template('form.html', params)
 
+
+from models import Contacts
+
+
+class ContactsHandler(BaseHandler):
+    def get(self, contact_id=None):
+        params = {
+            'page_title': 'Contacts'
+        }
+        if contact_id:
+            contact = Contacts.get_by_id(int(contact_id))
+        else:
+            contact = None
+        contacts = Contacts.query().order(-Contacts.created).fetch()
+        params["contacts"] = contacts
+        params["contact"] = contact
+        return self.render_template('contacts.html', params)
+
+    def post(self, contact_id=None):
+        first_name = self.request.get("first_name")
+        last_name = self.request.get("last_name")
+        email = self.request.get("email")
+        if contact_id:
+            msg = Contacts.get_by_id(int(contact_id))
+            msg.first_name = first_name
+            msg.last_name = last_name
+            msg.email = email
+        else:
+            msg = Contacts(first_name=first_name, last_name=last_name, email=email)
+        msg.put()
+        return self.redirect_to("contacts")
+
+
+class ContactDeleteHandler(BaseHandler):
+    def get(self, contact_id):
+        contact = Contacts.get_by_id(int(contact_id))
+        params = {"contact": contact}
+        return self.render_template("contact_delete.html", params=params)
+
+    def post(self, contact_id):
+        contact = Contacts.get_by_id(int(contact_id))
+        contact.key.delete()
+        return self.redirect_to('contacts')
+
+
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
     webapp2.Route('/about', AboutHandler),
     webapp2.Route('/form', FormHandler),
     webapp2.Route('/database', DatabaseHandler),
+    webapp2.Route('/contacts', ContactsHandler, name="contacts"),
+    webapp2.Route('/contacts/<contact_id:\d+>', ContactsHandler),
+    webapp2.Route('/contacts/delete/<contact_id:\d+>', ContactDeleteHandler),
 ], debug=True)
--- cloud/basic-gae-boilerplate/models.py
+++ cloud/basic-gae-boilerplate/models.py
@@ -1,6 +1,16 @@
 from google.appengine.ext import ndb
 
+
 class Message(ndb.Model):
     message_text = ndb.StringProperty()
     message_notes = ndb.StringProperty()
     created = ndb.DateTimeProperty(auto_now_add=True)
+    email = ndb.StringProperty()
+    skaitlis = ndb.IntegerProperty()
+
+
+class Contacts(ndb.Model):
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    first_name = ndb.StringProperty()
+    last_name = ndb.StringProperty()
+    email = ndb.StringProperty()
